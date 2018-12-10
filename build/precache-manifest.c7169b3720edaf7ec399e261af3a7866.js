73216f71eae49830092e18056fe96a7ae82f70b4
Juuso Korhonen <juseek@gmail.com>
2018-12-10 21:47:55 +0200

    proxy and api conf

---
+++ build/precache-manifest.c7169b3720edaf7ec399e261af3a7866.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ca86904c614235bf2193",
+    "url": "/static/js/main.ca86904c.chunk.js"
+  },
+  {
+    "revision": "097107a0fc6c17120c1f",
+    "url": "/static/js/1.097107a0.chunk.js"
+  },
+  {
+    "revision": "ca86904c614235bf2193",
+    "url": "/static/css/main.20c72b00.chunk.css"
+  },
+  {
+    "revision": "a8dfbc386cc3fdcf8aa0fd0cce739958",
+    "url": "/index.html"
+  }
+];
