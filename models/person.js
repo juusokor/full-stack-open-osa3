0ac9660a4726281162c80506ba0954d96f8ceba6
Juuso Korhonen <juseek@gmail.com>
2018-12-14 22:35:01 +0200

    refactor

--- models/person.js
+++ models/person.js
@@ -20,23 +20,6 @@ personSchema.statics.format = function(name) {
   };
 };
 
-personSchema.statics.hasName = function(body) {
-  Person.findOne({ name: body.name })
-    .then(result => {
-      console.log(result.name);
-      if (result.name === body.name) {
-        const answer = () => {
-          True;
-        };
-        return answer;
-      }
-      return { False };
-    })
-    .catch(error => {
-      console.log(error);
-    });
-};
-
 const Person = mongoose.model("Person", personSchema);
 
 module.exports = Person;
